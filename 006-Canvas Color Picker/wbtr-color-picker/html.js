7c953820ff61841fa5572a56c34141e96a236566
PardipLalWebgrity <152843184+PardipLalWebgrity@users.noreply.github.com>
2025-01-16 11:19:53 +0530

    Update html.js

--- 006-Canvas Color Picker/wbtr-color-picker/html.js
+++ 006-Canvas Color Picker/wbtr-color-picker/html.js
@@ -8,10 +8,10 @@ const html = `
 			</div>
 			<div class="cpicker-gradient" data-id="cpicker-gradient">
 				<canvas data-id="cpicker-gradient-canvas" width="271" height="32"></canvas>				
-				<input data-id="cpicker-gradient-input" type="range" min="0.00" max="1.00" step="0.01" value="0.66">
+				<input data-id="cpicker-gradient-input" type="range" min="0.00" max="1.00" step="0.01" value="0.66"> <!-- why min 0, max 1, i think 271 max -->
 			</div>
 			<div class="cpicker-transparent">				
-				<div class="overlay"></div>	
+				<div class="overlay" data-id="cpicker-transparent-overlay"></div>	
 				<input data-id="cpicker-transparent-input" type="range" min="0.00" max="1.00" step="0.01" value="0.5">
 			</div>
 
@@ -22,7 +22,7 @@ const html = `
 					<option value="css">css</option>
 				</select>
 				<div class="cpicker-code-inputs">
-					<div class="cpicker-code-input cpicker-code-hex">
+					<div class="cpicker-code-input cpicker-code-hex show">
 						<input type="text" data-id="cpicker-code-hex-input" placeholder="#000000">
 					</div>
 					<div class="cpicker-code-input cpicker-code-rgba">
@@ -31,7 +31,7 @@ const html = `
 						<input type="number" data-id="cpicker-code-rgba-binput" placeholder="255" step="1" min="0" max="255">
 						<input type="number" data-id="cpicker-code-rgba-ainput" placeholder="0.01" step="0.01" min="0" max="1">
 					</div>
-					<div class="cpicker-code-input cpicker-code-css show">
+					<div class="cpicker-code-input cpicker-code-css">
 						<input type="text" data-id="cpicker-code-css-input" placeholder="rgba(255, 255, 255, 0.99)">
 					</div>
 				</div>
